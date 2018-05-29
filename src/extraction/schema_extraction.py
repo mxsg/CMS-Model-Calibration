e1fc9a540edd79ef07992e6f14f76c2e9bb98654
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-05-29 09:32:42 +0200

    Analyze set of distinct nodes

--- src/extraction/schema_extraction.py
+++ src/extraction/schema_extraction.py
@@ -9,11 +9,14 @@ def main():
     wmarchive_record = ''
     with open('wmarchive_full_20180511_merged', 'r') as f:
         states = set()
+        hosts = set()
         for line in f:
             wmarchive_record = ast.literal_eval(line)
             states.add(wmarchive_record['meta_data']['jobstate'])
+            hosts.add(wmarchive_record['meta_data']['host'])
 
     print("States: {}".format(states))
+    print("States: {}".format(hosts))
 
     pretty_record = json.dumps(wmarchive_record, indent=4)
     print(pretty_record)
