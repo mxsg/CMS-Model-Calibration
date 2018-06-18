51f44b0c2b28d27e17a868ebd6ca3ec7a05837b3
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-18 08:34:05 +0200

    Add script to detect irregular CSV data in files

---
+++ src/utils/find_irregular_data.py
@@ -0,0 +1,29 @@
+import sys
+
+def main():
+    with open(sys.argv[1], 'r') as f:
+
+        header = f.readline();
+        header_names = header.split(',')
+
+        element_count = len(header_names)
+
+        print("Searching for Irregular data ...")
+        print("Header has {} elements".format(element_count))
+        print("Header: {}".format(header_names))
+
+        irregular_lines = 0
+
+        for i, line in enumerate(f):
+            line_elements = line.split(',')
+            if len(line_elements) != element_count:
+                irregular_lines += 1
+                print("--- Mismatched Line Found: Line {}".format(i))
+                print("number of elements: {}".format(line_elements))
+                for i, j in zip(header_names, line_elements):
+                    print("{}: {}".format(i, j))
+
+        print("Finished, found {} irregular lines.".format(irregular_lines))
+
+if __name__ == '__main__':
+    main()
