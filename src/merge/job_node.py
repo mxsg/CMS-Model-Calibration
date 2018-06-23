56199cb39dbc4c4b4f9d424cb3d3e7fdee9f2658
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-23 12:34:18 +0200

    Log number of unmatched job nodes

--- src/merge/job_node.py
+++ src/merge/job_node.py
@@ -13,8 +13,8 @@ def match_jobs_to_node(jobs: pd.DataFrame, nodes: pd.DataFrame):
     all_job_nodes = jobs.WNHostName.unique()
     available_hosts = nodes.hostname.unique()
 
-    logging.debug("Number of hosts jobs were run on: {}".format(all_job_nodes))
-    logging.debug("Number of hosts in resource environment: {}".format(available_hosts))
+    logging.debug("Number of hosts jobs were run on: {}".format(len(all_job_nodes)))
+    logging.debug("Number of hosts in resource environment: {}".format(len(available_hosts)))
 
     unmatched_job_nodes = [node for node in all_job_nodes if node not in available_hosts]
 
@@ -22,11 +22,18 @@ def match_jobs_to_node(jobs: pd.DataFrame, nodes: pd.DataFrame):
 
     logging.debug("Found {} nodes with jobs.".format(len([node for node in all_job_nodes if node in available_hosts])))
     logging.debug("Found {} unmatched job nodes: {}".format(len(unmatched_job_nodes), unmatched_job_nodes))
+
+    logging.debug("Number of jobs per unmatched job node:")
+    # Retrieve number of jobs that cannot be matched
+    logging.debug(", ".join(["node: {}, jobs: {}".format(node, jobs[jobs['WNHostName'] == node].shape[0]) for node in unmatched_job_nodes]))
+
     logging.debug("Found {} nodes without jobs.".format(len(nodes_without_jobs)))
 
     jobs_nodes = pd.merge(jobs, nodes, how='left', left_on='WNHostName', right_on='hostname')
 
     logging.debug("Joined {} job rows to {} node rows, result with {} rows"
                   .format(jobs.shape[0], nodes.shape[0], jobs_nodes.shape[0]))
+
+    # logging.debug("Jobs joined with nodes:\n" + str(jobs_nodes))
 
     return jobs_nodes
