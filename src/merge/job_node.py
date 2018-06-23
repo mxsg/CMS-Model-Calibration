0598caa7451f36b92650369cc756fd45a41dc485
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-23 11:46:33 +0200

    Add merging of jobs and nodes

---
+++ src/merge/job_node.py
@@ -0,0 +1,32 @@
+import logging
+
+import pandas as pd
+
+
+def match_jobs_to_node(jobs: pd.DataFrame, nodes: pd.DataFrame):
+    # kept_cols = ["WNHostName", "NCores", "Type", "WrapCPU", "WrapWC", "StartedRunningTimeStamp", "FinishedTimeStamp",
+    #                "JobType", "GenericType", "TaskMonitorId", "NEvReq", "NEvProc", "SubmissionTool",
+    #                "ApplicationVersion", "Application"]
+
+    # job_data = jobs[kept_cols]
+
+    all_job_nodes = jobs.WNHostName.unique()
+    available_hosts = nodes.hostname.unique()
+
+    logging.debug("Number of hosts jobs were run on: {}".format(all_job_nodes))
+    logging.debug("Number of hosts in resource environment: {}".format(available_hosts))
+
+    unmatched_job_nodes = [node for node in all_job_nodes if node not in available_hosts]
+
+    nodes_without_jobs = [node for node in available_hosts if node not in all_job_nodes]
+
+    logging.debug("Found {} nodes with jobs.".format(len([node for node in all_job_nodes if node in available_hosts])))
+    logging.debug("Found {} unmatched job nodes: {}".format(len(unmatched_job_nodes), unmatched_job_nodes))
+    logging.debug("Found {} nodes without jobs.".format(len(nodes_without_jobs)))
+
+    jobs_nodes = pd.merge(jobs, nodes, how='left', left_on='WNHostName', right_on='hostname')
+
+    logging.debug("Joined {} job rows to {} node rows, result with {} rows"
+                  .format(jobs.shape[0], nodes.shape[0], jobs_nodes.shape[0]))
+
+    return jobs_nodes
