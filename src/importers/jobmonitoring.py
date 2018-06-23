053748964b2f010a0d3256d3057047c5432a8e82
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-23 18:52:37 +0200

    Change JobMonitoring importer to convert time stamps

--- src/importers/jobmonitoring.py
+++ src/importers/jobmonitoring.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from .csv import CSVImporter
 
+
 class JobMonitoringImporter(CSVImporter):
 
     def __init__(self):
@@ -14,10 +15,9 @@ class JobMonitoringImporter(CSVImporter):
 
         # TODO Make these parameters
         self.dropped_columns = ['FileName', 'ProtocolUsed']
-        self.header =  'JobId,FileName,IsParentFile,ProtocolUsed,SuccessFlag,FileType,LumiRanges,StrippedFiles,BlockId,StrippedBlocks,BlockName,InputCollection,Application,ApplicationVersion,Type,GenericType,NewGenericType,NewType,SubmissionTool,InputSE,TargetCE,SiteName,SchedulerName,JobMonitorId,TaskJobId,SchedulerJobIdV2,TaskId,TaskMonitorId,NEventsPerJob,NTaskSteps,JobExecExitCode,JobExecExitTimeStamp,StartedRunningTimeStamp,FinishedTimeStamp,WrapWC,WrapCPU,ExeCPU,NCores,NEvProc,NEvReq,WNHostName,JobType,UserId,GridName'
+        self.header = 'JobId,FileName,IsParentFile,ProtocolUsed,SuccessFlag,FileType,LumiRanges,StrippedFiles,BlockId,StrippedBlocks,BlockName,InputCollection,Application,ApplicationVersion,Type,GenericType,NewGenericType,NewType,SubmissionTool,InputSE,TargetCE,SiteName,SchedulerName,JobMonitorId,TaskJobId,SchedulerJobIdV2,TaskId,TaskMonitorId,NEventsPerJob,NTaskSteps,JobExecExitCode,JobExecExitTimeStamp,StartedRunningTimeStamp,FinishedTimeStamp,WrapWC,WrapCPU,ExeCPU,NCores,NEvProc,NEvReq,WNHostName,JobType,UserId,GridName'
 
     def importDataFromFile(self, path):
-
         logging.info("Reading jobmonitoring file from {}".format(path))
 
         self.checkHeader(path, self.header)
@@ -38,12 +38,33 @@ class JobMonitoringImporter(CSVImporter):
         # logging.debug("Number of FileType entries: {}".format(df.FileType.unique()))
 
         self.regularizeHostNames(".gridka.de", df)
+        self.regularize_job_type(df)
+
+        # Convert to time stamps
+
+        time_stamp_columns = ['StartedRunningTimeStamp', 'FinishedTimeStamp', 'JobExecExitTimeStamp']
+
+        for col in time_stamp_columns:
+            # Filter out invalid time stamps and then find the first valid date in the data set
+            earliest_valid_epoch = df.loc[df[col] > 0, col].min()
+            earliest_datetime = pd.to_datetime(earliest_valid_epoch, unit='ms', origin='unix')
+
+            df[col] = pd.to_datetime(df[col], unit='ms', origin='unix')
+
+            # Reset invalid datetimes
+            df.loc[df[col] < earliest_datetime, col] = pd.NaT
+
+            # Count number of entries with invalid time stamps
+            logging.debug("Invalid with conversion to datetime {} count: {}".format(col, df[col].isnull().sum()))
+
+            logging.debug("Col {}: first date {}, last date {}".format(col, df[col].min(), df[col].max()))
+
+        logging.debug("Number of mismatching time stamps (Exit vs. Finished): {}"
+                      .format(df[df['FinishedTimeStamp'] != df['JobExecExitTimeStamp']].shape[0]))
 
         return df
 
-
     def regularizeHostNames(self, site_suffix, df):
-
         logging.debug("Regularizing Host Names")
 
         df['WNHostName.raw'] = df['WNHostName']
@@ -51,4 +72,7 @@ class JobMonitoringImporter(CSVImporter):
         df.WNHostName.replace('{}$'.format(site_suffix), '', regex=True, inplace=True)
 
         logging.debug("Host Name Count before {}, after {}"
-            .format(df['WNHostName.raw'].unique().shape[0], df.WNHostName.unique().shape[0]))
+                      .format(df['WNHostName.raw'].unique().shape[0], df.WNHostName.unique().shape[0]))
+
+    def regularize_job_type(self, df):
+        df['JobType'] = df['JobType'].str.lower()
