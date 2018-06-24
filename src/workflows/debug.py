71074074fa2f8302d7520dee9c4b5541fdf9927c
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-24 17:36:11 +0200

    Change debug workflow to incorporate new functionality

--- src/workflows/debug.py
+++ src/workflows/debug.py
@@ -1,17 +1,18 @@
-from analysis import nodeanalysis
-from analysis import jobmonitoring
+import logging
 
-from exporters import nodetypes
-from merge import job_node
+from analysis import demandextraction
+from analysis import jobmonitoring
+from analysis import jobtypesplit
+from analysis import nodeanalysis
 from importers.jobmonitoring import JobMonitoringImporter
 from importers.nodedata import GridKaNodeDataImporter
-
-import logging
+from merge import job_node
+from exporters import demandexport
 
 
 def run_workflow():
     jm_importer = JobMonitoringImporter()
-    jobs = jm_importer.importDataFromFile('../data/output_jobmonitoring_2018-03.txt')
+    jobs = jm_importer.importDataFromFile('../data/job_data.csv')
 
     node_importer = GridKaNodeDataImporter()
     nodes = node_importer.importDataFromFile('../data/gridka-benchmarks-2017.csv')
@@ -19,8 +20,22 @@ def run_workflow():
     nodeanalysis.addPerformanceData(nodes)
 
     matched_jobs = job_node.match_jobs_to_node(jobs, nodes)
+
     job_data = jobmonitoring.add_performance_data(matched_jobs)
 
+    demands = demandextraction.extract_demands(job_data)
 
+    demandexport.export_to_json_file(demands, '../out/jobs.json')
+
+    # Remove types that are very infrequent in the data
+    # job_data_filtered = jobmonitoring.filter_df_by_type(job_data, 0.0001)
+
+    # job_partitions = jobtypesplit.split_by_type(job_data_filtered, 'Type', True)
+    # demand_list = [demandextraction.extract_demands(partition) for partition in job_partitions]
+    #
+    # for demands in demand_list:
+    #     logging.debug("Demands:\n{}".format(demands))
+
+    # demandextraction.extract_demands(job_data)
 
 
