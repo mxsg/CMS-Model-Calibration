d59608b5403f6ae5b44ec21a131209afcb380599
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-24 17:31:15 +0200

    Add I/O during analysis

--- src/analysis/jobmonitoring.py
+++ src/analysis/jobmonitoring.py
@@ -1,6 +1,5 @@
 import logging
 
-import pandas as pd
 import numpy as np
 
 
@@ -11,15 +10,13 @@ def add_performance_data(df):
     job_type = 'Type'
     core_count = 'NCores'
 
-    # estimated_io_time = 'estimatedIOTime'
-    # estimated_io_time_total = 'estimatedIOTimeBusyTimeTotal'
-
     cpu_idle_time = 'CPUIdleTime'
     job_cpu_efficiency = 'OverallJobCPUEfficiency'
+    cpu_idle_time_ratio = 'CPUIdleTimeRatio'
 
+    # Do not copy for now
     job_data = df.copy()
 
-    # TODO Do this properly!
     logging.debug("WrapCPU summary: positive {}, zero {}, negative {}".format(job_data[job_data[wrap_cpu] > 0].shape[0],
                                                                               job_data[job_data[wrap_cpu] == 0].shape[
                                                                                   0],
@@ -33,11 +30,13 @@ def add_performance_data(df):
     logging.debug("NCores summary:\n" + job_data['NCores'].value_counts().to_string())
     logging.debug("Types summary:\n" + job_data[job_type].value_counts().to_string())
 
-    job_data[job_data[wrap_cpu] < 0] = pd.NaT
-    logging.debug("Number of NA values: {}".format(job_data[wrap_cpu].isnull().sum()))
+    # job_data[job_data[wrap_cpu] < 0][wrap_cpu] = np.nan
+    # logging.debug("Number of NA values: {}".format(job_data[wrap_cpu].isnull().sum()))
 
     job_data['CPUTimePerCore'] = job_data['WrapCPU'] / job_data['NCores']
     job_data['CPUDemand'] = job_data['WrapCPU'] * df['HSScorePerCore']
+
+    # return "Test2", job_data.dtypes
 
     job_data[cpu_idle_time] = job_data[wrap_wc] * job_data[core_count] - job_data[wrap_cpu]
 
@@ -48,6 +47,12 @@ def add_performance_data(df):
 
     max_cpu_time = job_data[wrap_wc] * job_data[core_count]
 
+    # TODO Is this valid?
+    # Set invalid values of CPU idle time to NaN
+    job_data.loc[(job_data[cpu_idle_time] < 0) | (job_data[cpu_idle_time] > max_cpu_time), cpu_idle_time] = np.nan
+
+    job_data[cpu_idle_time_ratio] = job_data[cpu_idle_time] / max_cpu_time.mask(max_cpu_time <= 0)
+
     # job_data[job_cpu_efficiency] = (job_data[wrap_cpu]).where(job_data[wrap_wc] > 0.0)
     job_data[job_cpu_efficiency] = job_data[wrap_cpu] / max_cpu_time.mask(max_cpu_time <= 0)
 
@@ -55,6 +60,13 @@ def add_performance_data(df):
     # job_data[job_cpu_efficiency].clip(lower=0.0, upper=1.0, inplace=true)
     # Clamp CPU efficiency by
     job_data.loc[(job_data[job_cpu_efficiency] < 0) | (job_data[job_cpu_efficiency] > 1), job_cpu_efficiency] = np.nan
-    logging.debug("Min job CPU efficiency: {}, max: {}".format(job_data[job_cpu_efficiency].min(), job_data[job_cpu_efficiency].max()))
+    logging.debug("Min job CPU efficiency: {}, max: {}".format(job_data[job_cpu_efficiency].min(),
+                                                               job_data[job_cpu_efficiency].max()))
 
     return job_data
+
+
+def add_io_data(df):
+    # estimated_io_time = 'estimatedIOTime'
+    # estimated_io_time_total = 'estimatedIOTimeBusyTimeTotal'
+    pass
