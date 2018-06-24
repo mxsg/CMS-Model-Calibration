0181a9e3038b536a283baf84d84c5ad660235f54
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-24 12:40:00 +0200

    Add methods to generate Stochastic Expressions

---
+++ src/utils/stoex.py
@@ -0,0 +1,32 @@
+def hist_to_doublepdf(counts, bins):
+    """
+    Converts a histogram to a double PDF Stochastic Expression.
+    """
+    if len(counts) + 1 != len(bins):
+        raise ValueError("Mismatching bin and value lengths for histogram.")
+
+    # Normalize counts to unity
+    relative_counts = counts / sum(counts)
+    right_edges = bins[1:]
+
+    keyword = "DoublePDF"
+
+    # TODO Was: "({:.8f};{:.8f})"
+    components = ['({};{})'.format(edge, count) for (count, edge) in zip(right_edges, relative_counts)]
+    return '{}[{}]'.format(keyword, ''.join(components))
+
+
+def to_intpmf(values, counts, simplify=True):
+    if len(counts) != len(values):
+        raise ValueError("Mismatching number of values and counts.")
+
+    if simplify == True & & len(values) == 1:
+        return values[0]
+
+    # Normalize counts to unity
+    relative_counts = counts / len(counts)
+
+    keyword = "IntPMF"
+
+    components = ['({};{})'.format(value, count) for (value, count) in zip(values, relative_counts)]
+    return '{}[{}]'.format(keyword, ''.join(components))
