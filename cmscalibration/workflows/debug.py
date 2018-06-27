30c068bc9a5a4c1c0ef2464b39bec37caad88fda
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-27 20:13:29 +0200

    Expand debug workflow

--- cmscalibration/workflows/debug.py
+++ cmscalibration/workflows/debug.py
@@ -1,20 +1,28 @@
 import logging
 
+import numpy as np
+import pandas as pd
+from analysis import cpuefficiencyanalysis
 from analysis import demandextraction
 from analysis import jobmonitoring
-from exporters import nodetypes
-from analysis import jobtypesplit
 from analysis import nodeanalysis
+from exporters import demandexport
+from exporters import nodetypes
+from importers.cpuefficiencies import CPUEfficienciesImporter
 from importers.jobmonitoring import JobMonitoringImporter
 from importers.nodedata import GridKaNodeDataImporter
+from importers.usedcores import CoreUsageImporter
 from merge import job_node
-from exporters import demandexport
 from validation import cpuefficiency
 
 
 def run_workflow():
     jm_importer = JobMonitoringImporter()
-    jobs = jm_importer.importDataFromFile('./data/output_jobmonitoring_2018-04.txt')
+
+    start_date = pd.to_datetime('2018-03-01')
+    end_date = pd.to_datetime('2018-05-01')
+
+    jobs = jm_importer.importDataFromFile('./data/output_jobmonitoring_2018-03to04.txt')
 
     node_importer = GridKaNodeDataImporter()
     nodes = node_importer.importDataFromFile('./data/gridka-benchmarks-2017.csv')
@@ -28,22 +36,59 @@ def run_workflow():
     cpu_efficiency = cpuefficiency.cpu_efficiency(job_data)
     logging.info("Total CPU time / Walltime efficiency: {}".format(cpu_efficiency))
     cpu_efficiency_scaled = cpuefficiency.cpu_efficiency_scaled_by_jobslots(job_data)
-    logging.info("Total CPU time / Walltime efficiency scaled by jobslot count: {}".format(cpu_efficiency_scaled))
+    logging.info("Total CPU time / Walltime efficiency scaled by jobslot count and virtual cores: {}".format(
+        cpu_efficiency_scaled))
 
+    cpu_efficiency_scaled_physical = cpuefficiency.cpu_efficiency_scaled_by_jobslots_physical(job_data)
+    logging.info("Total CPU time / Walltime efficiency scaled by jobslot count and physical cores: {}".format(
+        cpu_efficiency_scaled_physical))
+
+    core_importer = CoreUsageImporter()
+    core_usage_cms = core_importer.importDataFromFile('./data/core_usage_data_cms.csv')
+    total_cores = core_importer.importDataFromFile('./data/total_available_cores.csv')
+
+    core_usage_cms = core_usage_cms.rename(columns={'Value': 'CMSCores'})
+    total_cores = total_cores.rename(columns={'Value': 'TotalCores'})
+
+    logging.debug(core_usage_cms.columns)
+
+    core_df = core_usage_cms[['Timestamp', 'CMSCores']].merge(total_cores[['Timestamp', 'TotalCores']], on='Timestamp')
+    core_df['CMSShare'] = core_df['CMSCores'] / core_df['TotalCores']
+    core_df[core_df['CMSShare'] > 1] = np.nan
+
+    logging.info("Total mean share for CMS: {}".format(core_df['CMSShare'].mean()))
+    mean_core_share_in_timeframe = core_df[(core_df['Timestamp'] < end_date) & (core_df['Timestamp'])].mean()
+    logging.info(
+        "Mean share for CMS in time frame from {} to {}: {}".format(start_date, end_date, mean_core_share_in_timeframe))
+
+    cms_avg_cores = mean_core_share_in_timeframe['CMSCores']
+
+    logging.info("Mean number of slots for CMS: {}".format(cms_avg_cores))
 
     node_types = nodeanalysis.extractNodeTypes(nodes)
-    scaled_nodes = nodeanalysis.scaleSiteWithNodeTypes(node_types, 0.20)
+    scaled_nodes = nodeanalysis.scale_site_to_jobslot_count(node_types, cms_avg_cores)
     # scaled_nodes = nodeanalysis.scaleSiteWithNodeTypes(node_types, 0.20)
-
 
     out_directory = './out/params'
 
     nodetypes.exportToJsonFile(scaled_nodes, '/'.join([out_directory, 'nodes.json']))
 
-
     demands = demandextraction.extract_demands(job_data)
+    shares = {job_demands['typeName']: job_demands['relativeFrequency'] for job_demands in demands}
+
+    logging.debug(str(shares))
 
     demandexport.export_to_json_file(demands, '/'.join([out_directory, 'jobs.json']))
+
+    cpu_eff_importer = CPUEfficienciesImporter()
+    cpu_eff_df = cpu_eff_importer.importDataFromFile('./data/gridka_cpu_over_walltime.csv')
+
+    cpu_efficiency_data = cpuefficiencyanalysis.compute_average_cpu_efficiency(cpu_eff_df, start=start_date,
+                                                                               end=end_date)
+    # cpu_efficiency_data = cpuefficiencyanalysis.compute_average_cpu_efficiency(cpu_eff_df)
+    logging.debug(
+        "CPU Efficiency total from {} to {} (from GridKa perspective, with Pilots): {}".format(start_date, end_date,
+                                                                                               cpu_efficiency_data))
 
     # Remove types that are very infrequent in the data
     # job_data_filtered = jobmonitoring.filter_df_by_type(job_data, 0.0001)
@@ -55,5 +100,3 @@ def run_workflow():
     #     logging.debug("Demands:\n{}".format(demands))
 
     # demandextraction.extract_demands(job_data)
-
-
