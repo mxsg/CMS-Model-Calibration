6724b357abce6225fe0684807d751e47dbb8272d
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-01 16:25:14 +0200

    Use json config files

--- cmscalibration/utils/config.py
+++ cmscalibration/utils/config.py
@@ -1,19 +1,22 @@
-import configparser
-import os
+import json
 import logging
+import os
+
 
 def load_config(config_path):
-
     logging.debug("Loading configuration from path {}".format(config_path))
 
     if not os.path.isfile(config_path):
         logging.warning("Could not find configuration file in path {}".format(config_path))
         raise ValueError("Configuration file not found.")
 
-    parser = configparser.ConfigParser()
-    parser.read(config_path)
+    def load_key(key, dict, default=None):
+        globals()[key] = dict.get(key, default)
 
-    section = 'general'
+    config = json.load(config_path)
 
-    globals()['start_date'] = parser.get(section, 'start_date')
-    globals()['end_date'] = parser.get(section, 'end_date')
+    load_key('start_date', config)
+    load_key('end_date', config)
+
+    load_key('wma_input_files', config)
+    load_key('jm_input_files', config)
