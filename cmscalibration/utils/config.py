dd6106b0260e03e2b864eef25dd8b6f792927359
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-02 22:41:46 +0200

    Expand configuration file parsing

--- cmscalibration/utils/config.py
+++ cmscalibration/utils/config.py
@@ -2,6 +2,13 @@ import json
 import logging
 import os
 
+import pandas as pd
+
+# class InputFileSpec:
+#     def __init__(self, path, start_date, end_date):
+#         self.path = path
+#         self.start_date = pd.to_datetime(start_date)
+#         self.end_date = pd.to_datetime(end_date)
 
 def load_config(config_path):
     logging.debug("Loading configuration from path {}".format(config_path))
@@ -13,10 +20,18 @@ def load_config(config_path):
     def load_key(key, dict, default=None):
         globals()[key] = dict.get(key, default)
 
-    config = json.load(config_path)
+    with open(config_path) as file:
+        config = json.load(file)
 
     load_key('start_date', config)
     load_key('end_date', config)
 
-    load_key('wma_input_files', config)
-    load_key('jm_input_files', config)
+    jm_input = config.get('jm_input_files')
+    if jm_input is None:
+        jm_input = []
+        logging.warn("Configuration: No jm input files found!")
+
+
+    load_key('wma_input_dataset', config)
+    load_key('jm_input_dataset', config)
+    load_key('node_info', config)
