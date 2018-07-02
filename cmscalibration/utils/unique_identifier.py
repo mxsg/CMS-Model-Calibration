4745ed65a64e63a577c4e4799d193cef465d8fcb
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-02 22:47:04 +0200

    Add code to hash columns for creating a unique identifier

---
+++ cmscalibration/utils/unique_identifier.py
@@ -0,0 +1,23 @@
+import hashlib
+
+
+def hash_values(values):
+    h = hashlib.md5()
+
+    for value in values:
+        h.update(str(value).encode('utf-8'))
+
+    return h.hexdigest()
+
+
+def hash_row(row, columns):
+    return hash_values([row[column] for column in columns])
+
+
+def hash_builtin(row, columns):
+    return hash(tuple(row[column] for column in columns))
+
+
+def hash_columns(df, columns):
+    # return df.apply(lambda x: hash_values([x[column] for column in columns]), axis='columns')
+    return df.apply(lambda x: hash_builtin(x, columns), axis=1)
