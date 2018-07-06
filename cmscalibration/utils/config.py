4c681d7ca616033efb48cd7ac35f8142de123221
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-07 00:03:11 +0200

    Improve configuration error handling and add workflow config param

--- cmscalibration/utils/config.py
+++ cmscalibration/utils/config.py
@@ -2,13 +2,6 @@ import json
 import logging
 import os
 
-import pandas as pd
-
-# class InputFileSpec:
-#     def __init__(self, path, start_date, end_date):
-#         self.path = path
-#         self.start_date = pd.to_datetime(start_date)
-#         self.end_date = pd.to_datetime(end_date)
 
 def load_config(config_path):
     logging.debug("Loading configuration from path {}".format(config_path))
@@ -18,6 +11,8 @@ def load_config(config_path):
         raise ValueError("Configuration file not found.")
 
     def load_key(key, dict, default=None):
+        if key not in dict:
+            raise ValueError(f"Could not find required key {key}!")
         globals()[key] = dict.get(key, default)
 
     with open(config_path) as file:
@@ -26,12 +21,7 @@ def load_config(config_path):
     load_key('start_date', config)
     load_key('end_date', config)
 
-    jm_input = config.get('jm_input_files')
-    if jm_input is None:
-        jm_input = []
-        logging.warn("Configuration: No jm input files found!")
-
-
-    load_key('wma_input_dataset', config)
+    load_key('wm_input_dataset', config)
     load_key('jm_input_dataset', config)
     load_key('node_info', config)
+    load_key('workflow_module', config)
