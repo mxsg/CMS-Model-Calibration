fdc47560c2ab26af7c16bbbbd6f2d4176c566488
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-15 00:00:00 +0200

    Update validation package to use metrics

--- cmscalibration/validation/cpuefficiency.py
+++ cmscalibration/validation/cpuefficiency.py
@@ -1,12 +1,13 @@
+from data.dataset import Metric
+
+wrap_cpu = Metric.CPU_TIME.value
+wrap_wc = Metric.WALL_TIME.value
+
+core_count = Metric.USED_CORES.value
+cpu_time_per_core = 'CPUTimePerCore'
+
+
 def cpu_efficiency(df, include_zero_cpu=False):
-    required_columns = []
-
-    wrap_cpu = 'WrapCPU'
-    wrap_wc = 'WrapWC'
-
-    core_count = 'NCores'
-    cpu_time_per_core = 'CPUTimePerCore'
-
     df_filtered = df[df[wrap_wc] > 0]
 
     if include_zero_cpu:
@@ -20,15 +21,8 @@ def cpu_efficiency(df, include_zero_cpu=False):
 
     return total_cpu_time / total_walltime
 
+
 def cpu_efficiency_scaled_by_jobslots(df, include_zero_cpu=False):
-    required_columns = []
-
-    wrap_cpu = 'WrapCPU'
-    wrap_wc = 'WrapWC'
-
-    core_count = 'NCores'
-    cpu_time_per_core = 'CPUTimePerCore'
-
     df_filtered = df[df[wrap_wc] > 0]
 
     if include_zero_cpu:
@@ -37,21 +31,15 @@ def cpu_efficiency_scaled_by_jobslots(df, include_zero_cpu=False):
         df_filtered = df_filtered[df_filtered[wrap_cpu] > 0]
 
     # total_walltime = df_filtered[wrap_wc].dot(df_filtered[core_count])
-    total_walltime = (df_filtered[wrap_wc] * df_filtered[core_count] * df_filtered['coresLogical'] / df_filtered['jobslots']).sum()
+    total_walltime = (df_filtered[wrap_wc] * df_filtered[core_count] * df_filtered['coresLogical'] / df_filtered[
+        'jobslots']).sum()
 
     total_cpu_time = df_filtered[wrap_cpu].sum()
 
     return total_cpu_time / total_walltime
+
 
 def cpu_efficiency_scaled_by_jobslots_physical(df, include_zero_cpu=False):
-    required_columns = []
-
-    wrap_cpu = 'WrapCPU'
-    wrap_wc = 'WrapWC'
-
-    core_count = 'NCores'
-    cpu_time_per_core = 'CPUTimePerCore'
-
     df_filtered = df[df[wrap_wc] > 0]
 
     if include_zero_cpu:
@@ -60,9 +48,9 @@ def cpu_efficiency_scaled_by_jobslots_physical(df, include_zero_cpu=False):
         df_filtered = df_filtered[df_filtered[wrap_cpu] > 0]
 
     # total_walltime = df_filtered[wrap_wc].dot(df_filtered[core_count])
-    total_walltime = (df_filtered[wrap_wc] * df_filtered[core_count] * df_filtered['cores'] / df_filtered['jobslots']).sum()
+    total_walltime = (
+                df_filtered[wrap_wc] * df_filtered[core_count] * df_filtered['cores'] / df_filtered['jobslots']).sum()
 
     total_cpu_time = df_filtered[wrap_cpu].sum()
 
     return total_cpu_time / total_walltime
-
