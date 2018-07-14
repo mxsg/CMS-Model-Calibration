016ad91d10ca56fab919a66cb45901856fa55b59
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-15 00:00:00 +0200

    Restructure node type analysis

--- cmscalibration/analysis/nodeanalysis.py
+++ cmscalibration/analysis/nodeanalysis.py
@@ -13,7 +13,6 @@ def addPerformanceData(df):
     df['coresLogical'] = df.apply(lambda x: logical_cores(x['cores'], x['jobslots']), axis='columns')
 
 
-
 def extractNodeTypes(df):
     # columns that the data have to be grouped by to retrieve the different node types
     grouped_cols = ['cpu model',
@@ -39,7 +38,6 @@ def extractNodeTypes(df):
     logging.debug("Total physical cores in resource environment: {}".format(df['cores'].sum()))
     logging.debug("Total logical cores in resource environment: {}".format(df['coresLogical'].sum()))
 
-
     logging.debug("Node type summary:\n" + node_summary.to_string())
 
     return node_summary
@@ -52,70 +50,121 @@ def add_logical_core_count(df):
     df['coresLogical'] = df.apply(lambda x: logical_cores(x['cores'], x['jobslots']), axis='columns')
 
 
-def scale_site_to_jobslot_count(df, count):
-    total_slots = df['jobslots'].dot(df['nodeCount'])
+def scale_site_by_jobslots(df, count):
+    """
+    Scale a resource environment (data frame with node type information) to the supplied share. This method uses
+    the number of jobslots in each node as a target metric.
+    """
 
+    jobslot_col = 'jobslots'
+    count_col = 'nodeCount'
+
+    # Compute the total number of slots in the dataframe and the
+    total_slots = df['jobslots'].dot(df['nodeCount'])
     share = count / total_slots
 
-    logging.debug("Scaling site to jobslot count {} of total {}, share {}".format(count, total_slots, share))
-    return scaleSiteWithNodeTypes(df, share)
+    return scale_dataframe(df, share, count_col, jobslot_col)
 
-# TODO Change this to compute the best possible combination of machines to get as close to
-# the desired score as possible
-# TODO Also compute other delta values
-def scaleSiteWithNodeTypes(df, share, method='score'):
-    if share < 0.0 or share > 1.0:
-        raise ValueError('Share must be a value between 0.0 and 1.0.')
+
+def scale_site_by_benchmark(df, share, method='score'):
+    """
+    Scale a resource environment (data frame with node type information) to the supplied share.
+    The 'score' method uses benchmarking scores to scale the environment.
+    """
 
     if not method == 'score':
         raise ValueError('unknown scaling method')
 
-    logging.debug("Scaling site")
+    # TODO Replace these with generic values
+    benchmark_score_col = 'hs06'
+    node_count_col = 'nodeCount'
+
+    return scale_dataframe(df, share, node_count_col, benchmark_score_col)
+
+
+def scale_dataframe(dataframe, share, count_col, score_col):
+    """
+    Scale the dataframe: Create a new, modified data frame that contains values in its count_col column such that
+    the sum of the metric_col column is as close as possible to the proportion provided as a parameter.
+
+    :param dataframe: The data frame containing the original site configuration.
+    :param score_col: The column name of the metric that should be used as a target metric for scaling. This is being
+    summed for all rows in the data frame.
+    :param count_col: The column name of the column containing a count of the entities.
+    :param share: The target share of the sum of the metric column the data frame should be scaled to.
+    :return: A copied dataframe where the count_col entries are modified in such a way that the total sum of the metric
+    column is as close as possible to the share provided as a parameter.
+    """
+
+    total_score = 'temp_totalScore'
+    fractional_count = 'temp_fractional_count'
+    current_count = 'temp_current_count'
+    delta_score = 'temp_delta_score'
+
+    if share < 0.0 or share > 1.0:
+        raise ValueError('Share must be a value between 0.0 and 1.0.')
+
+    logging.debug("Scaling dataframe with metric {}, count column {} to share {}".format(score_col, count_col, share))
 
     # Create a local copy of the input data frame
-    node_df = df.copy()
-    node_df['totalScore'] = node_df['nodeCount'] * node_df['hs06']
+    df = dataframe.copy()
 
-    total_site_score = node_df['totalScore'].sum()
+    # The total contribution to the metric of a single row in the data frame
+    df[total_score] = df[count_col] * df[score_col]
 
-    desired_score = share * total_site_score
+    total_frame_score = df[total_score].sum()
+    desired_score = share * total_frame_score
 
-    logging.debug("total site score: {}".format(total_site_score))
-    logging.debug("Desired site score: {}".format(desired_score))
+    logging.debug("Total score: {}".format(total_frame_score))
+    logging.debug("Desired score: {}".format(desired_score))
 
-    node_df['fractionalCount'] = node_df['nodeCount'] * share
+    # Compute theoretical optimum scaled counts
+    df[fractional_count] = df[count_col] * share
 
-    node_df['currentCount'] = np.floor(node_df['fractionalCount']).astype(int)
+    # The rounded down counts will be used as a starting point for optimization of the metric to
+    # be as close as possible to the target metric
+    df[current_count] = np.floor(df[fractional_count]).astype(int)
 
-    node_df['deltaScore'] = node_df['hs06'] * (node_df['fractionalCount'] - node_df['currentCount'])
+    # Compute the delta between the optimal and current value
+    # Negative: the score contribution is too low
+    # Positive: the score contribution is too high
+    df[delta_score] = df[score_col] * (df[fractional_count] - df[current_count])
 
-    total_score_delta = node_df['deltaScore'].sum()
-    logging.debug("Total delta score: {}".format(total_score_delta))
+    total_score_delta = df[delta_score].sum()
+    logging.debug("Total score delta: {}".format(total_score_delta))
 
-    sorted_nodes = node_df.sort_values(by=['deltaScore'], ascending=False)
+    # TODO Future work: Optimize this to find an optimal solution, e.g. with dynamic programming
+
+    # Greedily increase the count for a row at a time until the score target is reached
+    sorted_nodes = df.sort_values(by=[delta_score], ascending=False)
 
     for index, row in sorted_nodes.iterrows():
         if total_score_delta <= 0.0:
+            # The target has been reached
             break
 
-        node_df.loc[index, 'currentCount'] += 1
-        total_score_delta -= node_df.loc[index, 'hs06']
+        df.loc[index, current_count] += 1
+        total_score_delta -= df.loc[index, score_col]
 
-    # Recompute delta scores
-    node_df['deltaScore'] = node_df['hs06'] * (node_df['fractionalCount'] - node_df['currentCount'])
-
-    current_scaled_score = node_df['hs06'].dot(node_df['currentCount'])
+    # Recompute delta scores and total scaled score
+    df[delta_score] = df[score_col] * (df[fractional_count] - df[current_count])
+    current_scaled_score = df[score_col].dot(df[current_count])
 
     logging.debug("Total score after scaling: {} (desired {}), score delta: {} ({} relative error)"
                   .format(current_scaled_score, desired_score, -total_score_delta, - total_score_delta / desired_score))
 
     result = df.copy()
-    result['nodeCount'] = node_df['currentCount']
-    result = result[result['nodeCount'] > 0]
+    result[count_col] = df[current_count]
+
+    # Only include rows with positive count
+    result = result[result[count_col] > 0]
 
     # Reindex resulting data frame
     result.index = range(len(result.index))
 
-    logging.debug("Scaled node environment:\n" + result.to_string())
+    # Drop temporary columns
+    result = result[dataframe.columns]
+
+    logging.debug("Scaled dataframe:\n" + result.to_string())
 
     return result
--- cmscalibration/analysis/sampling.py
+++ cmscalibration/analysis/sampling.py
@@ -1,14 +1,7 @@
-import pandas as pd
-import numpy as np
-
 def split_samples(df, frac=0.5, random_state=None):
     """
-    Splits the data frame into two parts where the first is about frac of the total size of the data frame.
-
-    :param df:
-    :param frac:
-    :param random_state:
-    :return:
+    Splits the data frame into two parts where the first is about the supplied fraction
+    of the total size of the data frame.
     """
 
     if random_state is None:
