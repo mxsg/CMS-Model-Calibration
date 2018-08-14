17b70bc04bbbdc95beabe67fb3145b07ee6e5710
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-08-14 17:40:12 +0200

    Expand report generation

--- cmscalibration/analysis/calibrationreport.py
+++ cmscalibration/analysis/calibrationreport.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.colors import LogNorm
 
 import utils.report as rp
-from analysis import cpuefficiency
+from analysis import cpuefficiency, resource_usage
 from data.dataset import Dataset, Metric
 
 
@@ -33,7 +34,7 @@ def jobtypes_over_time(dataset: Dataset):
 
     # Use crosstab to get pivot table with Job types in the columns
     df = pd.crosstab(index=[df[Metric.STOP_TIME.value]], columns=[df[Metric.JOB_TYPE.value]])
-    df = df.resample('1H').sum()
+    df = df.resample('1D').sum()
 
     # df2 = df.groupby(df[Metric.STOP_TIME.value].dt.hour).count()[Metric.JOB_TYPE.value]
     # pivot_df = df2.pivot(index=Metric.STOP_TIME.value, columns=Metric)
@@ -46,15 +47,49 @@ def jobtypes_over_time(dataset: Dataset):
     return fig, axes
     # fig.savefig(path)
 
-def demand_histogram(df):
+
+def demand_histogram(df, cutoff_quantile=0.98):
     x = df['CPUDemand']
     y = df['CPUIdleTime']
 
-    x_max = x.quantile(0.9)
-    y_max = y.quantile(0.9)
+    x_max = x.quantile(cutoff_quantile)
+    y_max = y.quantile(cutoff_quantile)
 
     fig, axes = plt.subplots()
-    axes.hist2d(x, y, bins=50, range=[[0, x_max], [0, y_max]])
+    counts, xedges, yedges, im = axes.hist2d(x, y, bins=50, range=[[0, x_max], [0, y_max]], norm=LogNorm())
+
+    # Add legend for the colors
+    fig.colorbar(im)
+
+    return fig, axes
+
+
+def jobslot_usage(df, resample_freq='1H'):
+    # Resample time series
+    jobslot_usage = df['totalSlots'].resample(resample_freq).mean()
+
+    # fig, axes = plt.subplots()
+    plt.figure()
+    axes = jobslot_usage.plot.line()
+    fig = axes.get_figure()
+    fig.tight_layout()
+
+    return fig, axes
+
+
+def multiple_jobslot_usage(series_dict, resample_freq='1H'):
+    fig, axes = plt.subplots()
+
+    for name, series in series_dict.items():
+        # Resample time series
+        usage_ts = series.resample(resample_freq).mean()
+        usage_ts.plot.line(ax=axes, label=name)
+
+    axes.set_xlabel('Time')
+    axes.set_ylabel('Allocated jobslots')
+    axes.legend()
+
+    fig.tight_layout()
 
     return fig, axes
 
@@ -94,16 +129,16 @@ def add_jobs_report_section(dataset: Dataset, report: rp.ReportBuilder):
 
     core_counts_per_workflow = df_filled.groupby(Metric.WORKFLOW.value)[Metric.USED_CORES.value].nunique()
 
-    # Filter out all workflows where all core counts are the same
-    multiple_core_counts = df_filled.groupby(Metric.WORKFLOW.value).filter(
-        lambda x: x[Metric.USED_CORES.value].nunique() != 1)
+    # # Filter out all workflows where all core counts are the same
+    # multiple_core_counts = df_filled.groupby(Metric.WORKFLOW.value).filter(
+    #     lambda x: x[Metric.USED_CORES.value].nunique() != 1)
 
     # add_frame_to_report(core_counts_per_workflow, report)
 
-    report.append("Overview over ")
-    add_frame_to_report(
-        multiple_core_counts.groupby([Metric.WORKFLOW.value, Metric.USED_CORES.value]).size().unstack(fill_value=0),
-        report)
+    # report.append("Overview over ")
+    # add_frame_to_report(
+    #     multiple_core_counts.groupby([Metric.WORKFLOW.value, Metric.USED_CORES.value]).size().unstack(fill_value=0),
+    #     report)
 
     category_summary = df.groupby(Metric.JOB_CATEGORY.value).size().reset_index()
 
@@ -158,12 +193,36 @@ def add_jobs_report_section(dataset: Dataset, report: rp.ReportBuilder):
     cpu_efficiencies = df.groupby(Metric.JOB_TYPE.value).apply(cpuefficiency.cpu_efficiency)
     add_frame_to_report(cpu_efficiencies, report)
 
-
     report.append("### Job Demands")
 
-    jobs_analysis = df[(df[Metric.JOB_TYPE.value] == 'reprocessing') & (df['CPUDemand'].notnull()) & (df['CPUIdleTime'].notnull())]
-    fig, axes = demand_histogram(jobs_analysis)
-    report.add_figure(fig, axes, 'job_demands_analysis')
+    job_types = df[Metric.JOB_TYPE.value].unique()
+
+    job_type_groups = df.groupby(Metric.JOB_TYPE.value)
+
+    for job_type, jobs in job_type_groups:
+        report.append("CPU Demand and Idle Time for jobs of type {}".format(job_type))
+        jobtype_df = jobs[(jobs['CPUDemand'].notnull()) & (jobs['CPUIdleTime'].notnull())]
+        fig, axes = demand_histogram(jobtype_df)
+        report.add_figure(fig, axes, 'job_demands_{}'.format(job_type))
+
+    report.append("#### Jobslot usage overview")
+    report.append()
+
+    mean_jobslots = resource_usage.mean_jobslot_usage(df, dataset.start, dataset.end,
+                                                      start_ts_col=Metric.START_TIME.value,
+                                                      end_ts_col=Metric.STOP_TIME.value,
+                                                      slot_col=Metric.USED_CORES.value)
+
+    jobslot_timeseries = resource_usage.calculate_jobslot_usage(df, dataset.start, dataset.end,
+                                                                start_ts_col=Metric.START_TIME.value,
+                                                                end_ts_col=Metric.STOP_TIME.value,
+                                                                slot_col=Metric.USED_CORES.value)
+    report.append("Mean number of jobslots used: {}  ".format(mean_jobslots))
+
+    report.append("Jobslot usage over time:")
+
+    fig, axes = jobslot_usage(jobslot_timeseries)
+    report.add_figure(fig, axes, 'jobslot_usage')
 
 
 def add_frame_to_report(df, report: rp.ReportBuilder):
