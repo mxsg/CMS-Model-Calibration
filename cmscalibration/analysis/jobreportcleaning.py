2b5e24d3b62a52f8f8eb36b969b0a61cdf5e1bae
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-08-17 14:30:59 +0200

    Fill in core counts by heuristics

--- cmscalibration/analysis/jobreportcleaning.py
+++ cmscalibration/analysis/jobreportcleaning.py
@@ -6,9 +6,10 @@ from data.dataset import Metric
 
 
 def clean_job_reports(df):
-    df = _add_missing_core_counts(df)
     df = _add_missing_walltimes(df)
     df = _add_missing_jobtypes(df)
+    df = _core_thread_count_heuristic(df)
+    # df = _add_missing_core_counts(df)
     return df
 
 
@@ -33,13 +34,13 @@ def _add_missing_core_counts(df):
         else:
             return series
 
-    # grouped_metrics = [Metric.WORKFLOW.value, Metric.JOB_CATEGORY.value]
-    grouped_metrics = [Metric.WORKFLOW.value]
+    grouped_metrics = [Metric.WORKFLOW.value, Metric.JOB_CATEGORY.value]
+    # grouped_metrics = [Metric.WORKFLOW.value]
 
     df_filled[Metric.USED_CORES.value] = df_filled.groupby(grouped_metrics)[Metric.USED_CORES.value].transform(
         fill_unique)
 
-    # Todo Enable filling of used core counts
+    # # Todo Enable filling of used core counts
     # df_filled[Metric.USED_CORES.value] = df_filled.groupby(Metric.WORKFLOW.value)[Metric.USED_CORES.value].transform(
     #     lambda x: x.fillna(x.median()))
 
@@ -75,6 +76,74 @@ def _add_missing_core_counts(df):
         df_filled[Metric.USED_CORES.value].isnull().sum()))
 
     return df_filled
+
+
+def _core_thread_count_heuristic(df: pd.DataFrame):
+    df = df.copy()
+
+    # Find job categories that are always single-threaded
+    # Note: This might change in the future or in other data sets
+
+    singlethreaded_categories = ['harvesting', 'logcollect', 'merge']
+    mask = df[Metric.JOB_CATEGORY.value].isin(singlethreaded_categories)
+
+    df.loc[mask, Metric.USED_CORES.value] = 1
+    df.loc[mask, Metric.USED_THREADS.value] = 1
+
+    # Fill in jobs with number of used cores of 1 and 2, which are identical in their
+    # numbers of threads and streams
+    direct_fill = [1.0, 2.0]
+
+    # Find out position where either of the metrics is set to the directly filled value
+    masks = {value: (df[Metric.USED_CORES.value] == value) | (df[Metric.USED_THREADS.value] == value) for value in
+             direct_fill}
+
+    # Now set appropriate values
+    for value, mask in masks.items():
+        df.loc[mask, Metric.USED_CORES.value] = value
+        df.loc[mask, Metric.USED_THREADS.value] = value
+
+    df = _advanced_heuristics_thread_count(df)
+
+    return df
+
+
+# Todo Be careful, these might be wrong!
+def _advanced_heuristics_thread_count(df: pd.DataFrame):
+    logging.debug("Filling in missing core count information (same thread + core count), missing before {}".format(
+        df[Metric.USED_CORES.value].isnull().sum()))
+
+    df = df.copy()
+
+    # In most cases, thread and core counts are identical
+    replace_cores = df[Metric.USED_CORES.value].isnull() & df[Metric.USED_THREADS.value].notnull()
+    replace_threads = df[Metric.USED_CORES.value].notnull() & df[Metric.USED_THREADS.value].isnull()
+
+    df.loc[replace_cores, Metric.USED_CORES.value] = df.loc[replace_cores, Metric.USED_THREADS.value]
+    df.loc[replace_threads, Metric.USED_THREADS.value] = df.loc[replace_threads, Metric.USED_CORES.value]
+
+    logging.debug("Filling in missing core count information (same thread + core count), missing after {}".format(
+        df[Metric.USED_CORES.value].isnull().sum()))
+
+    # Filling in missing counts with median from the group
+    both_missing = df[Metric.USED_CORES.value].isnull() & df[Metric.USED_THREADS.value].isnull()
+
+    # grouped_dict = {Metric.WORKFLOW.value: '#unknown', Metric.JOB_CATEGORY.value: '#unknown',
+    #                 Metric.JOB_TYPE.value: '#unknown'}
+
+    grouped_dict = {Metric.JOB_CATEGORY.value: '#unknown',
+                    Metric.JOB_TYPE.value: '#unknown'}
+
+    df_filled = df.fillna(grouped_dict)
+    median_filled_cores = df_filled.groupby(list(grouped_dict.keys()))[Metric.USED_CORES.value].transform(
+        lambda x: x.fillna(x.median()))
+
+    df.loc[both_missing, Metric.USED_CORES.value] = median_filled_cores.loc[both_missing]
+
+    logging.debug("Filling in missing core count information (fill median), missing after {}".format(
+        df[Metric.USED_CORES.value].isnull().sum()))
+
+    return df
 
 
 def _add_missing_jobtypes(df: pd.DataFrame):
