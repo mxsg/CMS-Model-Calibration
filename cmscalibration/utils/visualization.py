b9ec03f4de85547bd368680d24e46de2a14f700c
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-08-21 16:28:56 +0200

    Add possibility to create overview plots for multiple job types

--- cmscalibration/utils/visualization.py
+++ cmscalibration/utils/visualization.py
@@ -1,34 +1,61 @@
-import numpy as np
-import numpy as np
-
 import matplotlib.pyplot as plt
 
 
 def draw_binned_data(counts, bins):
     fig, axes = plt.subplots()
 
+    draw_binned_data_subplot(counts, bins, axes)
+
+    return fig, axes
+
+
+def draw_binned_data_subplot(counts, bins, axes, name=''):
     centroids = (bins[1:] + bins[:-1]) / 2
+    total = sum(counts)
 
     axes.hist(centroids, density=True, bins=bins, weights=counts, histtype='step')
 
     axes.set_xlim(left=0)
     axes.set_ylim(bottom=0)
 
-    return fig, axes
+    axes.set_title("{} (from {} jobs)".format(name, total))
+
+    return axes
 
 
-def draw_integer_distribution(values, counts):
+def draw_integer_distribution(values, counts, norm=True, name=''):
     fig, axes = plt.subplots()
 
-    min_value = np.min(values)
-    max_bin = np.max(values)
+    # min_value = np.min(values)
+    # max_bin = np.max(values)
 
-    bins = np.arange(min_value, max_bin + 1)
-    vals = np.zeros(max_bin - min_value + 1)
+    # bins = np.arange(min_value, max_bin + 1)
+    # vals = np.zeros(max_bin - min_value + 1)
 
-    for k, v in zip(values, counts):
-        vals[k - min_value] = v
+    # for k, v in zip(values, counts):
+    #     vals[k - min_value] = v
 
-    axes.bar(bins, vals)
+    axes = draw_integer_distribution_subplot(values, counts, axes, norm, name)
 
     return fig, axes
+
+
+def draw_integer_distribution_subplot(values, counts, axes, norm=True, name=''):
+    total = sum(counts)
+    shares = [x / total for x in counts]
+
+    # Either draw total number or shares
+    bars = shares if norm else counts
+
+    axes.bar(values, bars)
+
+    axes.set_title("{} (from {} jobs)".format(name, total))
+
+    axes.set_xlabel(r"Number of required slots")
+    if norm:
+        axes.set_ylabel("Share")
+        axes.set_ylim(0, 1)
+    else:
+        axes.set_ylabel("Number of Jobs")
+
+    return axes
