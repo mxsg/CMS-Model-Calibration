42cd9c6815eed985568954e5a90cdf3cd62dae2e
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-08-30 00:09:40 +0200

    Clean up main module

--- cmscalibration/__main__.py
+++ cmscalibration/__main__.py
@@ -1,64 +1,77 @@
 #!/usr/bin/env python3
-
-import importlib
+import argparse
 import logging
 import os
 import sys
+import traceback
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
 
+from interfaces.workflow import CalibrationWorkflow
 from utils import config
 
 
 def main():
-    setup_logging()
+    """Main function for the model calibration tool."""
 
-    conf_path = 'calibration.json'
-    param_count = len(sys.argv) - 1
+    parser = argparse.ArgumentParser("Extract calibration parameters from CMS monitoring data.")
+    parser.add_argument("--conf", default="calibration.json",
+                        help="Path to the configuration file used for calibration")
 
-    # Check for supplied configuration file or standard configuration file location
-    if param_count > 1:
-        print("Please provide a single configuration file path.")
-        print("You can also supply a configuration in the standard location '{}'".format(conf_path))
-        sys.exit(1)
-
-    if param_count == 1:
-        conf_path = sys.argv[1]
+    args = parser.parse_args()
 
     # Load configuration file
     try:
-        config.load_config(conf_path)
+        config.load_config(args.conf)
     except ValueError as e:
+        # Directly print as logging infrastructure is not set up yet
         print("Could not load configuration file. Error: {}".format(e))
         print("Exiting.")
         sys.exit(1)
 
-    logging.debug("Starting Model Calibration.")
+    log_subdir = os.path.join(config.output_directory)
+    setup_logging(log_subdir)
+
+    logging.info("Starting Model Calibration.")
 
     logging.debug("Running with Pandas version: {}".format(pd.__version__))
     logging.debug("Running with Numpy version: {}".format(np.__version__))
 
     # Load workflow module from configuration file
+    def import_class(name):
+        modname, classname = name.rsplit('.', 1)
+        module = __import__(modname, fromlist=[classname])
+        attr = getattr(module, classname)
+        return attr
+
     try:
-        workflow_module = importlib.import_module(config.workflow_module)
+        workflow_class = import_class(config.workflow)
     except ImportError as e:
-        print("Could not import workflow module {}. Error: {}".format(config.workflow_module, str(e)))
+        logging.error("Could not import workflow class {}. Error: {}".format(config.workflow, str(e)))
         sys.exit(1)
 
-    if not hasattr(workflow_module, 'run'):
-        print("Could not run workflow. Workflows must implement a run() method.")
+    if not issubclass(workflow_class, CalibrationWorkflow):
+        logging.error("Workflows must implement the CalibrationWorkflow interface.")
         sys.exit(1)
-    else:
-        workflow_module.run()
 
-    logging.debug("Model Calibration Finished")
+    # Run workflow
+    workflow = workflow_class()
+    try:
+        workflow.run()
+    except Exception:
+        logging.error("An error occured during execution of the workflow!")
+        logging.error(traceback.format_exc())
+        sys.exit(1)
+
+    logging.info("Model Calibration Finished")
 
 
-def setup_logging():
+def setup_logging(log_path: str):
+    """Setup the logging files at the specified path."""
+
     # Setup logging to file and the console
-    log_path = "log"
     os.makedirs(log_path, exist_ok=True)
 
     # Log into file with current date and time
